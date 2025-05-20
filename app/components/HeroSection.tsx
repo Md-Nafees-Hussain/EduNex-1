ef42626d06d9920dc91f9bb2811ca0725f85f92a
Akhlaque Ur Rahman <ikhlakhrehman@gmail.com>
2025-05-20 23:51:34 +0530

    image changed

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -75,7 +75,7 @@ export default function HeroSection() {
 
   <div className="w-full aspect-[16/9] relative bg-white p-4">
     <Image
-  src="/resources/community-home.jpg"
+  src="/resources/Community-Home.jpg"
   alt="EduNex dashboard preview"
   fill
   priority // 🚀 Improve LCP (above-the-fold)
